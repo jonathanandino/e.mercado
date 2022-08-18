4a454eb9198097c221e657a6025316d16b4568ab
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-08-18 19:02:33 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -12,3 +12,33 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+// Escribe en el menu dependiendo de la sesion
+function cargarInicio(){
+
+    let botonInicio = "";
+if (localStorage.getItem("usuario")){
+    if (localStorage.getItem("usuario")== "visitante" ){
+
+        window.location.href= "login.html"
+        botonInicio += '<a class="btn btn-primary" href="login.html" role="button">Iniciar Secion</a>'
+        document.getElementById("inicio").innerHTML = botonInicio;  
+        
+    }else{
+        var Bienvenida = '<a class="binevenida" >Usuario: '+localStorage.getItem("usuario")+' </a>'
+        botonInicio +=  '<a class="btn btn-primary" onclick="salirUsuario()" role="button">salir</a>'        
+        document.getElementById("inicio").innerHTML = botonInicio;
+        document.getElementById("usuario").innerHTML = Bienvenida; 
+
+    }
+    // redirecciona al login en caso que el localSronge = null
+}else{
+    window.open("login.html","_self");
+}
+}
+// funcion para cerrar seccion quedando el usuario como visitante
+function salirUsuario(){
+    let user= 'visitante'
+    localStorage.setItem("usuario", user);
+    location.reload();
+}
