b60d8d13765980ceeab2fd874b909f9ed3b233cb
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-08-20 05:32:53 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -19,3 +19,9 @@ function onSignIn(googleUser) {
     alert("bienvenido"); 
     Windows.location.href("index.html");
   }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
