7e20fb0dbfcd7d70c32e59005148287df29b49c6
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-08-20 04:48:22 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -12,5 +12,12 @@ function comprobarValores() {
     }else{
         alert("Por favor complete los campos")
     }
-}; 
-
+};
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    localStorage.setItem("usuario",profile.getName())
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
