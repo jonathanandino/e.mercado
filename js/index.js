9d84136f92f72490768021ee1e9052ab97eaa573
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-09-05 11:58:35 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,39 +13,3 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-// Escribe en el menu dependiendo del usuario - (si - usuario == x) muestra usuario  
-function cargarInicio(){
-    let botonInicio = "";
-
-    if (localStorage.getItem("usuario")){
-        if (localStorage.getItem("usuario")== "visitante" ){
-
-            window.location.href= "login.html"
-            botonInicio += '<a class=" btn-primary" href="login.html" role="button">Iniciar Secion</a>'
-            document.getElementById("usuario").innerHTML = botonInicio;  
-            
-        }else{
-
-            var Bienvenida = '<a class="binevenida" >Usuario: <b>'+localStorage.getItem("usuario")+'</b> </a>'
-            botonInicio +=  '<a class="btn dropdown-item " onclick="salirUsuario()" >Salir</a>'        
-            document.getElementById("inicio").innerHTML = botonInicio;
-            document.getElementById("usuario").innerHTML = Bienvenida; 
-
-        }
-        // redirecciona al login en caso que usuario == null
-    }else{
-        window.open("login.html","_self");
-    }
-}
-// funcion para cerrar seccion quedando el usuario == visitante
-    function salirUsuario(){
-        let user= 'visitante';
-
-        localStorage.setItem("usuario", user);
-        location.reload();
-    }
-// salir de google
-    function signOut() {
-        google.accounts.id.disableAutoSelect();
-        location.reload();
-}
--- js/product-info.js
+++ js/product-info.js
@@ -8,12 +8,17 @@ function cargarTitulo(){
       let  datos =`
 
                   <div class="single-product-details">
-                    <h2>`+data.name+`</h2>
-                    <h3 class="product-price">`+data.currency+` `+data.cost+`</h3>
+                    <h2><b>`+data.name+`</b></h2>
+                    <p class="product-description mt-20" style="color:green"><b>`+data.soldCount+` vendidos</b></p>
+                    <h5 class="product-price"><b>`+data.currency+` `+data.cost+`</b></h5><br>
                     <p class="product-description mt-20">`+data.description+`</p>
-                    <div class="product-category"> Categoria:`+data.category+`</div>
-                    <div class="text-center"><a href="" class="btn btn-primary ">Comprar</a></div>
-                  </div>
+                    <div class="product-category" style="color:grey"> Categoria: <b>`+data.category+`</b></div><br>
+                    <div class="text-center" 
+                    <div ><button type="button" class="btn btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>  Añadir al carrito</button><a href="" style="background-color:black; color:white" class="btn"><b>Comprar</b></a></div>
+                    </div>
+                    </div>
                 
         `
 
@@ -27,7 +32,6 @@ function cargarTitulo(){
         <img src="`+imagen+`" class="d-block w-100" alt="..."></div>`
         boton += `
         <button type="button" class="`+active+`" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="`+i+`" aria-label="Slide `+i+1+`"></button>`
-        console.log(active)
         active = "" 
     }
     let dat = data.relatedProducts;
@@ -36,7 +40,7 @@ function cargarTitulo(){
         let relacionado = dat[i];
 
         rel += `
-        <div class="col-md-5">
+        <div class="col-md-5 card cursor-active list-group-item-actio" onclick="setProID(`+relacionado.id+`)">
             <div class="product-item card h-100">
                 <div class="product-thumb">
                     <img class="card-img-top" src="`+relacionado.image+`" alt="product-img" />
@@ -49,7 +53,6 @@ function cargarTitulo(){
         </div>`
     }
      
-     console.log(dat.length)
     document.getElementById("dato").innerHTML = datos;
     document.getElementById("rela").innerHTML= rel;
     document.getElementById("bot").innerHTML = boton;
@@ -57,4 +60,8 @@ function cargarTitulo(){
     })
     }
     
+    function setProID(id) {
+        localStorage.setItem("ProID", id);
+        window.location = "product-info.html"
+    }
     cargarTitulo()
