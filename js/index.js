b68f943f2bf1bee1fd4bdbb82555f184a47f342c
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-09-20 07:00:08 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -25,3 +25,13 @@ document.addEventListener("DOMContentLoaded", function(){
         });
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
