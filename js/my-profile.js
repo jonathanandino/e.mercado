b3fab21334baeccb84600c0f9d0ad16a0c72504b
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-11-08 06:14:14 -0300

    Add files via upload

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", function(){
             document.getElementById("picture").innerHTML = `<img  id="menuImg" src="`+usuario[0].picture+`" width="150" height="150" class="zoom rounded-circle"></img>`
             
         }
-        if (usuario[0].given_name){
-            document.getElementById("name").setAttribute("value", usuario[0].given_name)
+        if (usuario[0].givenName){
+            document.getElementById("name").setAttribute("value", usuario[0].givenName)
         }
         if (usuario[0].family_name){
-            document.getElementById("lastName").setAttribute("value", usuario[0].family_name)
+            document.getElementById("lastName").setAttribute("value", usuario[0].lastName)
         }
         if (usuario[0].email){
             document.getElementById("email").setAttribute("value", usuario[0].email)
