3b4df255f93e945728a4b398d407fe9f653a5b48
jonathanandino <110565501+jonathanandino@users.noreply.github.com>
2022-11-08 06:24:57 -0300

    Add files via upload

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function(){
         var usuario = JSON.parse(user);
 
         if(usuario[0].picture){
-            document.getElementById("picture").innerHTML = `<img  id="menuImg" src="`+usuario[0].picture+`" width="150" height="150" class="zoom rounded-circle"></img>`
+            document.getElementById("image").innerHTML = `<img  id="menuImg" src="`+usuario[0].picture+`" width="150" height="150" class="zoom rounded-circle"></img>`
             
         }
         if (usuario[0].givenName){
             document.getElementById("name").setAttribute("value", usuario[0].givenName)
         }
-        if (usuario[0].family_name){
+        if (usuario[0].lastName){
             document.getElementById("lastName").setAttribute("value", usuario[0].lastName)
         }
         if (usuario[0].email){
